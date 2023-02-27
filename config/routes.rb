d3b168d9a8ccf8ac4924307839931e0dd10b602b
terusibata <Oharatomoki4546@icloud.com>
2023-02-28 00:00:23 +0900

    メニューにroutesを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'top_pages#home'
+  get    '/signup', to: 'users#new'
   get    '/login',  to: 'sessions#new'
   post   '/login',  to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
